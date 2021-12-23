7874d221065efe1cd17301e503836fc1d7acb57b
Ugo Clemente <ugoclementecronache@gmail.com>
2021-12-23 10:35:30 +0100

    Modificato il file promise_all.js. Ora funziona

--- ownpromise2.js
+++ ownpromise2.js
@@ -1,58 +1,41 @@
-// let verso1 = "<h1>Nel mezzo del cammin di nostra vita</h1>";
-// let verso2 = "<h1>mi ritrovai per una selva oscura</h1>";
-// let verso3 = "<h1>ché la diritta via era smarrita...</h1>";
+// Creo un h1 e lo appendo al body
 let p = document.createElement("h1");
-let verso1 = document.createTextNode("Nel mezzo del cammin di nostra vita ");
-let verso2 = document.createTextNode("mi ritrovai per una selva oscura ");
-let verso3 = document.createTextNode("ché la diritta via era smarrita...");
-p.appendChild(verso1);
-p.appendChild(verso2);
-p.appendChild(verso3);
 document.body.appendChild(p);
-// let divina = [verso1, verso2, verso3];
-// for(var i = 1; 1 < 4; i++){
-//     document.write(divina[i]);
-// };
+// creo un array di stringhe e dichiaro una variabile "nodoverso"
+let verso1 = "Nel mezzo del cammin di nostra vita ";
+let verso2 = "mi ritrovai per una selva oscura ";
+let verso3 = "ché la diritta via era smarrita...";
+let versi = [verso1, verso2, verso3];
+let nodoverso;
 
-// function setup(){
+// Questa è la funzione setup, che fa partire le funzioni promise
+function setup(){
+    // Qui creo un array di promises, ne faccio partire 3
+    // e con Promise.all chiedo di attendere la risoluzione 
+    // di tutte le promises
+    let promises = [ritardo(1000), ritardo(1500), ritardo(2000)];
+    Promise.all(promises)
+    .then(() => {
+        // Quando le promises sono completate tutte appendo
+        // i nodi di testo al nodo h1, tutti insieme
+        for(var i = 0; i < 3; i++){
+            nodoverso = document.createTextNode(versi[i]);
+            p.appendChild(nodoverso);
+        };
+    })
+    .catch((err) => console.error(err));
+}
 
-//     let promises = [ritardo(1000), ritardo(1500), ritardo(2000)];
-//     Promise.all(promises)
-//     .then(() => {
-//         for(var i = 1; 1 < 4; i++){
-//             document.write(divina[i]);
-//         };
-//     })
-//     .catch((err) => console.error(err));
-// }
-
-//     ritardo(1000)
-//     .then(() => {
-//         document.write("<h1>Nel mezzo del cammin di nostra vita</h1>");
-//     })
-//     .catch((err) => console.error(err));
-    
-//     ritardo(1500)
-//     .then(() => {
-//         document.write("<h1>mi ritrovai per una selva oscura</h1>");
-//     })
-//     .catch((err) => console.error(err));
-
-//     ritardo(2000)
-//     .then(() => {
-//         document.write("<h1>ché la diritta via era smarrita...</h1>");
-//     })
-//     .catch((err) => console.error(err));
-// }
-
-// function ritardo(tempo){    
-//     return new Promise((resolve, reject) => {
-//         if(isNaN(tempo)){
-//             reject(new Error('Ma che stai a ffaaaaa???'));
-//         }
-//         setTimeout(resolve,tempo);
+// Questa è la funzione della singola promise
+function ritardo(tempo){    
+    return new Promise((resolve, reject) => {
+        if(isNaN(tempo)){
+            reject(new Error('Ma che stai a ffaaaaa???'));
+        }
+        // La promise attende un intervallo temporale per la risoluzione
+        setTimeout(resolve,tempo);
         
-//     });
-// }
+    });
+}
 
-// setup();
+setup();
--- promise_all.js
+++ promise_all.js
@@ -1,39 +1,41 @@
-var api = "https://api.giphy.com/v1/gifs/search?";
-var apiKey = "&api_key=A3cpWCs7nNWi59E0m0759qG22codT9Ze";
-var query = "&q=";
-var url = api + apiKey + query;
-var boredApi = "https://www.boredapi.com/api/activity"
-var ilmioHTML
+// Creo un h1 e lo appendo al body
+let p = document.createElement("h1");
+document.body.appendChild(p);
+// creo un array di stringhe e dichiaro una variabile "nodoverso"
+let verso1 = "Nel mezzo del cammin di nostra vita ";
+let verso2 = "mi ritrovai per una selva oscura ";
+let verso3 = "ché la diritta via era smarrita...";
+let versi = [verso1, verso2, verso3];
+let nodoverso;
 
+// Questa è la funzione setup, che fa partire le funzioni promise
 function setup(){
-    wordGIF()
-    .then(results => {
-        ilmioHTML = '<h1>' + results.parolout + '</h1>';
-        ilmioHTML += '<img src="' + results.fotout + '" alt="boh" width="500px"></img>';
-        document.body.innerHTML = ilmioHTML;
-        return(wordGIF());
+    // Qui creo un array di promises, ne faccio partire 3
+    // e con Promise.all chiedo di attendere la risoluzione 
+    // di tutte le promises
+    let promises = [ritardo(1000), ritardo(1500), ritardo(2000)];
+    Promise.all(promises)
+    .then(() => {
+        // Quando le promises sono completate tutte appendo
+        // i nodi di testo al nodo h1, tutti insieme
+        for(var i = 0; i < 3; i++){
+            nodoverso = document.createTextNode(versi[i]);
+            p.appendChild(nodoverso);
+        };
     })
-    .then(results => {
-        ilmioHTML += '<h1>' + results.parolout + '</h1>';
-        ilmioHTML += '<img src="' + results.fotout + '" alt="boh" width="500px"></img>';
-        document.body.innerHTML = ilmioHTML;
-    })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 }
 
-async function wordGIF (){
-    let response1 = await fetch(boredApi);
-    let json1 = await response1.json();
-    let parola = json1.type;
-    let response2 = await fetch(url + parola);
-    let json2 = await response2.json();
-    let tettuccio = json2.data.length; 
-    let casuale = Math.floor(Math.random() * tettuccio);
-    let foto = json2.data[casuale].images['fixed_height_small'].url;
-    return{
-        parolout:parola,
-        fotout:foto
-    }
+// Questa è la funzione della singola promise
+function ritardo(tempo){    
+    return new Promise((resolve, reject) => {
+        if(isNaN(tempo)){
+            reject(new Error('Ma che stai a ffaaaaa???'));
+        }
+        // La promise attende un intervallo temporale per la risoluzione
+        setTimeout(resolve,tempo);
+        
+    });
 }
 
 setup();
